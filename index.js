3bf1dcdf428ad00805ed66d5c04f80a327fe72c0
Johanna Farace <Johanna.farace@gmail.com>
2018-01-09 18:07:34 +0000

    get post and update route

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080,
+    bodyParser = require('body-parser');
+    
+var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.send('Hi there from express');
+});
+
+app.use('/api/todos', todoRoutes);
+
+app.listen(port, function(){
+    console.log("I'm listening on " + port);
+})
