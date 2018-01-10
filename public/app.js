991e99af7ff192cdff06ee34214bf2e4a8489d1c
Johanna Farace <Johanna.farace@gmail.com>
2018-01-10 13:53:03 +0000

    fetch todos using json api and append to html

--- public/app.js
+++ public/app.js
@@ -1,1 +1,11 @@
-// alert('hello from app.js');
+$(document).ready(function(){
+    $.getJSON('/api/todos')
+    .then(addTodos)
+});
+
+function addTodos(todos){
+    todos.forEach(function(todo){
+        var newTodo = $('<li class="task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    }); 
+}
